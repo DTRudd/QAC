91ecf3d474872d707c75ded1e550b63bd5c44875
M1ch43lGr33n <green.michael333@googlemail.com>
2017-07-10 14:59:32 +0100

    added a basic footer

--- scr/components/App.jsx
+++ scr/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
+import Footer from './Footer';
 
 class App extends React.Component {
   render() {
     return (
       <div>
 		<Nav />
+		<Footer />
       </div>
     );
   }
