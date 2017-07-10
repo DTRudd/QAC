8728e054e75de4061df591da986e010aa4139aa6
EMHankombo <enochmh2@hotmail.com>
2017-07-10 17:16:02 +0000

    Information,Price and FAQ page

---
+++ scr/components/InformationPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Price from './Price';
+import FAQ from './FAQ';
+
+export default class InformationPage extends React.Component{
+    
+    
+    render(){
+    return(
+<div>
+<h1>marc</h1> 
+        <Price />
+		<FAQ />
+		
+    </div>
+    
+    );    
+    }
+}
