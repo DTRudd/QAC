942133f25763f26483567b9d9f8afa75cce2279b
M1ch43lGr33n <green.michael333@googlemail.com>
2017-07-10 16:51:43 +0100

    updated website

---
+++ scr/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class Footer extends React.Component{
+
+	render(){
+		return(
+			<div>
+				<a href="localhost:8080/">Home</a>
+				<p></p>
+				<a href="">Films</a>
+				<p></p>
+				<a href="">Find us</a>
+				<p></p>
+				Information
+					- <a href="">Prices</a>
+					- <a href="">FAQ's </a>
+					- <a href="">Contact</a>
+				<p></p>
+				<a href="">Log In</a>
+				<p></p>
+				Email : <a href="">cinema@Email.co.uk</a>
+			</div>
+		);
+	}
+}
