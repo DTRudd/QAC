2b63ee7b24f753bada47f706f65936c0da587d98
Marc Edees <Marc@Marcs-MacBook-Pro.local>
2017-07-10 15:00:52 +0100

    price + info

---
+++ scr/components/InformationPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Price from './Price';
+
+export default class InformationPage extends React.Component{
+    
+    
+    render(){
+    return(
+<div>
+<p>marc</p> 
+        <Price />
+    </div>
+    
+    );    
+    }
+}
