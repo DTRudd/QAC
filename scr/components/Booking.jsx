0480c97cc2a0f0eebe3bcfcdebf21e67f70cafbf
msedees1807 <marc_edees@hotmail.co.uk>
2017-07-11 11:37:21 +0100

    Update Booking.jsx

--- scr/components/Booking.jsx
+++ scr/components/Booking.jsx
@@ -4,8 +4,8 @@ export default class Booking extends React.Component {
   render() {
     return (
 	<div>
-      <h2>Booking Page</h2>
-    </div>
+      		<h2>Booking Page</h2>
+   	</div>
 	);
   }
 }
