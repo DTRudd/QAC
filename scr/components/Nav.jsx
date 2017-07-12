3936e343f250f218f9237991768e8ae528fe38e8
msedees1807 <marc_edees@hotmail.co.uk>
2017-07-12 16:56:18 +0100

    Updated NAV bar to contain 'create acc' and home

--- scr/components/Nav.jsx
+++ scr/components/Nav.jsx
@@ -7,22 +7,29 @@ export default class Nav extends React.Component{
     return(
       <header className="mdl-layout__header" id="menu">
         <nav className="mdl-layout__header-row">
-          <h1 className="mdl-layout-title">QA Cinemas</h1>
-          <nav className="mdl-navigation">
-            <IndexLink to="/" className="pageLink mdl-navigation__link" activeClassName="active">Home</IndexLink>
+            
+          <IndexLink to="/" className="pageLink mdl-navigation__link" activeClassName="active">QA </IndexLink>
+          
+            
+            <nav className="mdl-navigation">
+            <Link to="/Films" className="pageLink mdl-navigation__link" activeClassName="active">Films</Link>
             <Link to="/FindUs" className="pageLink mdl-navigation__link" activeClassName="active">Find us</Link>
-            <Link to="/Booking" className="pageLink mdl-navigation__link" activeClassName="active">Booking</Link>
+          
             <span id="info-menu-lower-left" className="mdl-js-button mdl-navigation__link">Information</span>
             <ul className="mdl-menu mdl-menu--buttom-left mdl-js-menu" htmlFor="info-menu-lower-left">
-              <li><Link to="/Prices" className="pageLink mdl-menu__item" activeClassName="active">Pricing</Link></li>
+              <li><Link to="/Price" className="pageLink mdl-menu__item" activeClassName="active">Pricing</Link></li>
               <li><Link to="/FAQ" className="pageLink mdl-menu__item" activeClassName="active">FAQs</Link></li>
               <li><Link to="/Contact" className="pageLink mdl-menu__item" activeClassName="active">Contact</Link></li>
             </ul>
-            <Link to="/Films" className="pageLink mdl-navigation__link" activeClassName="active">Films</Link>
+            <Link to="/Booking" className="pageLink mdl-navigation__link" activeClassName="active">Booking</Link>
+
             <Link to="/Login" className="pageLink mdl-navigation__link" activeClassName="active"><button className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">Log in</button></Link>
+           
+                <Link to="/Login" className="pageLink mdl-navigation__link" activeClassName="active"><button className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">Create Account</button></Link>
           </nav>
         </nav>
       </header>
     );
   }
 }
+
