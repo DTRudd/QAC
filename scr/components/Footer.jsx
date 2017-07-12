90f3673d37e0da0b7b07741a71d292875577f826
Sophie Rudd <DTRudd@outlook.com>
2017-07-12 16:11:13 +0100

    branch mini footer

--- scr/components/Footer.jsx
+++ scr/components/Footer.jsx
@@ -2,38 +2,38 @@ import React from 'react';
 import {Link} from 'react-router';
 export default class Footer extends React.Component{
 
-	render(){
-		return(
-			<footer className="mdl-mega-footer">
-				<div className="mdl-mega-footer__drop-down-section">
-					<h4 className="mdl-mega-footer__heading">QA Cinemas</h4>
-					<ul className="mdl-mega-footer__link-list">
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/Films">Films</Link></li>
-						<li><Link to="/FindUs">Find us</Link></li>
-					</ul>
-				</div>
-				<div className="mdl-mega-footer__drop-down-section">
-					<h4 className="mdl-mega-footer__heading">Information</h4>
-					<ul className="mdl-mega-footer__link-list">
-						<li><Link to="/Prices">Prices</Link></li>
-						<li><Link to="/FAQ">FAQs</Link></li>
-						<li><Link to="/Contact">Contact</Link></li>
-					</ul>
-				</div>
-				<div className="mdl-mega-footer__drop-down-section">
-					<h4 className="mdl-mega-footer__heading">Account</h4>
-					<ul className="mdl-mega-footer__link-list">
-						<li><Link to="/Login">Log In</Link></li>
-					</ul>
-				</div>
-				<div className="mdl-mega-footer__drop-down-section">
-					<h4 className="mdl-mega-footer__heading">Contact</h4>
-					<ul className="mdl-mega-footer__link-list">
-						<li>Email : <a href="mailto:cinema@email.co.uk">cinema@email.co.uk</a></li>
-					</ul>
-				</div>
-			</footer>
-		);
-	}
+  render(){
+    return(
+      <footer className="mdl-mega-footer">
+        <div className="mdl-mega-footer__drop-down-section">
+          <div className="mdl-mega-footer__heading">QA Cinemas</div>
+          <ul className="mdl-mega-footer__link-list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Films">Films</Link></li>
+            <li><Link to="/FindUs">Find us</Link></li>
+          </ul>
+        </div>
+        <div className="mdl-mega-footer__drop-down-section">
+          <div className="mdl-mega-footer__heading">Information</div>
+          <ul className="mdl-mega-footer__link-list">
+            <li><Link to="/Prices">Prices</Link></li>
+            <li><Link to="/FAQ">FAQs</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+          </ul>
+        </div>
+        <div className="mdl-mega-footer__drop-down-section">
+          <div className="mdl-mega-footer__heading">Account</div>
+          <ul className="mdl-mega-footer__link-list">
+            <li><Link to="/Login">Log In</Link></li>
+          </ul>
+        </div>
+        <div className="mdl-mega-footer__drop-down-section">
+          <div className="mdl-mega-footer__heading">Contact</div>
+          <ul className="mdl-mega-footer__link-list">
+            <li>Email : <a href="mailto:cinema@email.co.uk">cinema@email.co.uk</a></li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
 }
