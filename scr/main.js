dc36c8dda3f37535f188ad11f4d2d4fa88af93dd
Marc Edees <Marc@Marcs-MacBook-Pro.local>
2017-07-12 16:52:08 +0100

    improved bars

--- scr/main.js
+++ scr/main.js
@@ -9,6 +9,7 @@ import FAQ from './components/FAQ';
 import Films from './components/Films';
 import InformationPage from './components/InformationPage';
 import Login from './components/Login';
+import Price from './components/Price';
 
 ReactDOM.render(
 	<Router history={browserHistory} >
@@ -21,6 +22,7 @@ ReactDOM.render(
 			<Route path="/Films" component={Films} />
 			<Route path="/Info" component={InformationPage} />
 			<Route path="/Login" component={Login} />
+            <Route path = "/Price" component = {Price} />
 		</Route>
 	</Router>,
    document.querySelector('#app')
