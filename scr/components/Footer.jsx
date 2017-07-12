cdd4dc792ba271402fbbc3463459664e13f5ee74
Sophie Rudd <DTRudd@outlook.com>
2017-07-12 16:17:59 +0100

    Changed to mini footer

--- scr/components/Footer.jsx
+++ scr/components/Footer.jsx
@@ -4,32 +4,13 @@ export default class Footer extends React.Component{
 
   render(){
     return(
-      <footer className="mdl-mega-footer">
-        <div className="mdl-mega-footer__drop-down-section">
-          <div className="mdl-mega-footer__heading">QA Cinemas</div>
-          <ul className="mdl-mega-footer__link-list">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/Films">Films</Link></li>
+      <footer className="mdl-mini-footer">
+        <div className="mdl-mini-footer__left-section">
+          <div className="mdl-logo">QA Cinemas</div>
+          <ul className="mdl-mini-footer__link-list">
             <li><Link to="/FindUs">Find us</Link></li>
-          </ul>
-        </div>
-        <div className="mdl-mega-footer__drop-down-section">
-          <div className="mdl-mega-footer__heading">Information</div>
-          <ul className="mdl-mega-footer__link-list">
-            <li><Link to="/Prices">Prices</Link></li>
             <li><Link to="/FAQ">FAQs</Link></li>
             <li><Link to="/Contact">Contact</Link></li>
-          </ul>
-        </div>
-        <div className="mdl-mega-footer__drop-down-section">
-          <div className="mdl-mega-footer__heading">Account</div>
-          <ul className="mdl-mega-footer__link-list">
-            <li><Link to="/Login">Log In</Link></li>
-          </ul>
-        </div>
-        <div className="mdl-mega-footer__drop-down-section">
-          <div className="mdl-mega-footer__heading">Contact</div>
-          <ul className="mdl-mega-footer__link-list">
             <li>Email : <a href="mailto:cinema@email.co.uk">cinema@email.co.uk</a></li>
           </ul>
         </div>
