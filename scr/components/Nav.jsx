36cf90a7604be5b5bf6ebe18cdf0c14f53b25f08
Sophie Rudd <DTRudd@outlook.com>
2017-07-12 12:03:39 +0100

    Working drop-down

--- scr/components/Nav.jsx
+++ scr/components/Nav.jsx
@@ -10,12 +10,10 @@ export default class Nav extends React.Component{
           <h1 className="mdl-layout-title">QA Cinemas</h1>
           <nav className="mdl-navigation">
             <IndexLink to="/" className="pageLink mdl-navigation__link" activeClassName="active">Home</IndexLink>
-            <Link to="/Index1" className="pageLink mdl-navigation__link" activeClassName="active">Index Page</Link>
-            <Link to="/Index2" className="pageLink mdl-navigation__link" activeClassName="active">Index</Link>
             <Link to="/FindUs" className="pageLink mdl-navigation__link" activeClassName="active">Find us</Link>
             <Link to="/Booking" className="pageLink mdl-navigation__link" activeClassName="active">Booking</Link>
             <span id="info-menu-lower-left" className="mdl-js-button mdl-navigation__link">Information</span>
-            <ul className="mdl-menu mdl-menu--buttom-left mdl-js-menu" for="info-menu-lower-left">
+            <ul className="mdl-menu mdl-menu--buttom-left mdl-js-menu" htmlFor="info-menu-lower-left">
               <li><Link to="/Info" className="pageLink mdl-menu__item" activeClassName="active">Information</Link></li>
               <li><Link to="/FAQ" className="pageLink mdl-menu__item" activeClassName="active">FAQs</Link></li>
               <li><Link to="/Contact" className="pageLink mdl-menu__item" activeClassName="active">Contact</Link></li>
