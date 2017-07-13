b132b59bdaba3c6faa9af8f45b7823914b58dda4
M1ch43lGr33n <green.michael333@googlemail.com>
2017-07-13 10:16:09 +0100

    setting up my own branch, will merge with dev at points

---
+++ scr/components/Account.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Account extends React.Component {
+  render() {
+    return (
+	<div>
+      <h2>Account Page</h2>
+	  links to create account page and edit details.
+	</div>
+	);
+  }
+}
