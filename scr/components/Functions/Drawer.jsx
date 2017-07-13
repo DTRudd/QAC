ed022a801f1b31d4d5e73d5ddd47df4a2957a195
Sophie Rudd <DTRudd@outlook.com>
2017-07-13 14:00:24 +0100

    Improved drawer

--- scr/components/Functions/Drawer.jsx
+++ scr/components/Functions/Drawer.jsx
@@ -10,12 +10,9 @@ export default class Nav extends React.Component{
         <nav className="mdl-navigation">
           <Link to="/Films" className="pageLink mdl-navigation__link" activeClassName="active">Films</Link>
           <Link to="/FindUs" className="pageLink mdl-navigation__link" activeClassName="active">Find us</Link>         
-          <span id="info-menu-lower-left" className="mdl-js-button mdl-navigation__link">Information</span>
-          <ul className="mdl-menu mdl-menu--buttom-left mdl-js-menu" htmlFor="info-menu-lower-left">
-            <li><Link to="/Prices" className="pageLink mdl-menu__item" activeClassName="active">Pricing</Link></li>
-            <li><Link to="/FAQ" className="pageLink mdl-menu__item" activeClassName="active">FAQs</Link></li>
-            <li><Link to="/contact" className="pageLink mdl-menu__item" activeClassName="active">Contact</Link></li>
-          </ul>
+          <Link to="/Prices" className="pageLink mdl-navigation__link" activeClassName="active">Pricing</Link>
+          <Link to="/FAQ" className="pageLink mdl-navigation__link" activeClassName="active">FAQs</Link>
+          <Link to="/contact" className="pageLink mdl-navigation__link" activeClassName="active">Contact</Link>
           <Link to="/Booking" className="pageLink mdl-navigation__link" activeClassName="active">Booking</Link>
           <div className="mdl-layout-space"></div>
           <Link to="/Login" className="pageLink mdl-navigation__link" activeClassName="active"><button className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">Log in</button></Link>           
