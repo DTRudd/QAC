696bff5dd3a6b14dfb7132709d5db209b90c707a
Sophie Rudd <DTRudd@outlook.com>
2017-07-13 09:51:08 +0100

    Films component is grid

--- scr/components/FilmTile.jsx
+++ scr/components/FilmTile.jsx
@@ -4,8 +4,8 @@ export default class FilmTile extends React.Component{
 
 	render(){
 		return(
-			<div>
-				Film Tile: Contains image and title.
+			<div className="mdl-cell">
+				<table><tbody><tr><td>Film Tile: Contains image and title.</td></tr></tbody></table>
 			</div>
 		);
 	}
