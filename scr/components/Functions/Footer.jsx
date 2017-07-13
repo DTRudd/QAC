86733c601d559d7d536b21d2e5d561a0aeebec8b
Sophie Rudd <DTRudd@outlook.com>
2017-07-13 17:01:00 +0100

    Minimal footer

--- scr/components/Functions/Footer.jsx
+++ scr/components/Functions/Footer.jsx
@@ -16,23 +16,15 @@ export default class Footer extends React.Component{
     return(
       <footer className="mdl-mini-footer">
         <div className="mdl-mini-footer__left-section">
-          <div className="mdl-logo">QA Cinemas</div>
           <ul className="mdl-mini-footer__link-list">
-            <li><Link to="/FindUs">Find us</Link></li>
-            <li><Link to="/FAQ">FAQs</Link></li>
-            <li><Link to="/Contact">Contact</Link></li>
-            <li>Email : <a href="mailto:cinema@email.co.uk">cinema@email.co.uk</a></li>
+            <li><select className="mdl-textfield__input" value={this.state.value} onChange={this.handleChange}>
+                  {this.props.films.map((film) => 
+                    <option className="mdl-textfield__input" key={film.id} value={film.film_name}>{film.film_name}</option>
+		  )}</select></li>
           </ul>
         </div>
         <div className="mdl-mini-footer__right-section">
           <ul className="mdl-mini-footer__link-list">
-            <li>
-              <select value={this.state.value} onChange={this.handleChange}>
-                {this.props.films.map((film) => 
-                  <option key={film.id} value={film.film_name}>{film.film_name}</option>
-		)}
-               </select>
-            </li>
             <li>
               <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">Book now!</button>
             </li>
