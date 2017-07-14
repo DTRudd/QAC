5d6e1d01d6184295d1293359c9325e167110f1a2
Sophie Rudd <dtrudd@outlook.com>
2017-07-14 09:13:06 +0000

    case

---
+++ scr/components/pages/Contacts.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class contact extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Contacts page</h4>
+        <div>
+          <p>Our email: Cinema@Email.com </p>
+          <p>Phone number: 010203 04050607 </p>
+        </div>
+      </div>
+    );
+  }
+}
