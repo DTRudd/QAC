4b98a685ac7f45260b1fb90426c91fac8521c531
Sophie Rudd <dtrudd@outlook.com>
2017-07-14 11:35:30 +0000

    Added date to footer

--- scr/components/pages/Home.jsx
+++ scr/components/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import filmsList from './../../../res/Films.json';
+import filmsList from './../../../res/films.json';
 import NumberList from './../functions/NumberList'
 
 export default class Home extends React.Component {
