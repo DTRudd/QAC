3e148f442dc538601f6ea5215fb3561b3275e5d1
M1ch43lGr33n <green.michael333@googlemail.com>
2017-07-17 16:16:59 +0100

    alterations to pop up info and required css

--- scr/components/functions/PopUpInfo.jsx
+++ scr/components/functions/PopUpInfo.jsx
@@ -12,12 +12,16 @@ export default class PopUpInfo extends React.Component{
 	
 	render(){
 		return(
-		<div className="Drop-down-information"> 
-			<p>{this.props.film_name}</p>
-			<p>{this.props.film_description}</p>
-			<Link to="/Login" activeClassName="active">
-				<button className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" > Book now </button>
-			</Link>
+		<div className="popUpInfoBox">
+			<div className="Pop-Up-information"> 
+				<p>{this.props.film_name}</p>
+				<p>{this.props.film_description}</p>
+				<Link to="/Login" activeClassName="active">
+					<button className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" > Book now </button>
+				</Link>
+				<h1>Trailer goes here</h1>
+				<button onClick={this.props.onClose}> close </button>
+			</div>
 		</div>
 	);
 	}
--- scr/components/functions/Sitemap.jsx
+++ scr/components/functions/Sitemap.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 
 export default class Sitemap extends React.Component{
-	
-	render(){
-		return(
-			<div>
-				<ul className="sitemap-footer-list">
-					<li>Home</li>
-					<li>Films</li>
-					<li>Find us</li>
-					<li>Information
-						<ul className="footer-sub-list">
-							<li>Price</li>
-							<li>FAQ</li>
-							<li>Contacts</li>
-						</ul>
-					</li>
-					<li>Bookings</li>
-					<li>LogIn</li>
-				</ul>
-			</div>
-		);
-	}
+
+  render(){
+    return(
+      <div className="mdl-mega-footer">
+        <div className="mdl-mega-footer__drop-down-section">
+          <h1 className="mdl-mega-footer__heading">Information</h1>
+          <ul className="mdl-mega-footer__link-list">
+            <li>Price</li>
+            <li>FAQ</li>
+            <li>Contacts</li>
+            <li>Find us</li>
+          </ul>
+        </div>
+        <div className="mdl-mega-footer__drop-down-section">
+          <h1 className="mdl-mega-footer__heading">Cinemas</h1>
+          <ul className="mdl-mega-footer__link-list">
+            <li>Home</li>
+            <li>Films</li>
+            <li>Bookings</li>
+          </ul>
+        </div>
+        <div className="mdl-mega-footer__drop-down-section">
+          <h1 className="mdl-mega-footer__heading">Account Features</h1>
+          <ul className="mdl-mega-footer__link-list">
+            <li>Log in</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
