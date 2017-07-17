412c1638ce30fc74a242e1e78b9b297f6a13a6cd
Sophie Rudd <dtrudd@outlook.com>
2017-07-17 14:57:04 +0100

    fixed-location mega footer

--- scr/components/functions/ListItem.jsx
+++ scr/components/functions/ListItem.jsx
@@ -17,17 +17,20 @@ export default class ListItem extends React.Component{
   }
   
   render(){
-    return (    
-      <div className="mdl-cell mdl-card" style={{width:"255px"}} onClick={this.onSelect.bind(this)}>
-        <div className="mdl-card__media">
-          <img id="1" className="pic"  src={this.props.film.img}/>
+    return (
+      <div className="mdl-cell mdl-cell--4-col">
+        <div className="mdl-card" style={{width:"255px"}} onClick={this.onSelect.bind(this)}>
+          <div className="mdl-card__media">
+            <img id="1" className="pic"  src={this.props.film.img}/>
+          </div>
+          <div className="mdl-card__title">  
+            <h4 className="mdl-card__title-text">{this.props.film.film_name}</h4>
+            <img id="2" height="30px" width="30px" className="age-rating-logo" src={this.props.film.age_rating}/>
+          </div>
+          <div className="mdl-card__supporting-text">{this.props.film.quote}</div>
+          {this.state.isActive ? <DropDownInfo film_name={this.props.film.film_name} film_description={this.props.film.film_description}/> : ''}
         </div>
-        <div className="mdl-card__title">  
-          <h4 className="mdl-card__title-text">{this.props.film.film_name}</h4>
-          <img id="2" height="30px" width="30px" className="age-rating-logo" src={this.props.film.age_rating}/>
-        </div>
-        <div className="mdl-card__supporting-text">{this.props.film.quote}</div>
-        {this.state.isActive ? <DropDownInfo film_name={this.props.film.film_name} film_description={this.props.film.film_description}/> : ''}
+          <div className="mdl-spacer"></div>
       </div>
     );
   }
--- scr/components/functions/NumberList.jsx
+++ scr/components/functions/NumberList.jsx
@@ -7,9 +7,11 @@ import ListItem from './ListItem';
     return (
     <div className="mdl-grid">
       {films.map((film) =>
-      <ListItem key={film.id}
-             film={film} />
+        <div className="mdl-cell--col-4">
+          <ListItem key={film.id}
+            film={film} />
+        </div>
       )}
     </div>
-    );
-  }
+  );
+}
