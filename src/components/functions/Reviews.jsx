8b81606c9be6db3df7580059333786d1fd10d26b
M1ch43lGr33n <green.michael333@googlemail.com>
2017-07-19 11:56:10 +0100

    moved comments to seperate JSON, and added correct code to run from this JSON

--- src/components/functions/Reviews.jsx
+++ src/components/functions/Reviews.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReviewsItem from './ReviewsItem';
+import ReviewsList from '../../json/Reviews.json';
 
 export default class Reviews extends React.Component{
 	
 	constructor(props){
 		super(props)
 		this.state={
-			inputValue:'Please leave a comment',
+			inputValue:'',
+			ReviewsList:ReviewsList,
+			id:this.props.id,
 		}
 	}
 	
 	returnMethod(){
-		const comments = this.props.comments;
+		const comments = this.state.ReviewsList.ReviewsList[this.props.id -1].comments;
 		return(
 		<ul className="demo-list-three mdl-list">
 			  {comments.map((comments) =>
@@ -25,6 +28,12 @@ export default class Reviews extends React.Component{
 	updateInputValue(e){
 		this.setState({inputValue: e.target.value})
 	}
+	
+	updateJSONFile(){
+		var str = JSON.stringify(ReviewsList.ReviewsList[this.props.id-1].comments);
+		console.log(str)
+		//send input value in to JSON file.
+	}
 
 	render(){
 		return(
@@ -32,8 +41,8 @@ export default class Reviews extends React.Component{
 			{this.returnMethod()}
 				<div className="ReviewCommentSection" >
 					<input type="text" value={this.state.inputValue}
-						onChange={this.updateInputValue.bind(this)} />
-					<button > Submit </button>
+						onChange={this.updateInputValue.bind(this)} placeholder="Please leave a comment"/>
+					<button onClick={this.updateJSONFile.bind(this)}> Submit </button>
 				</div>
 			</div>
 		);
