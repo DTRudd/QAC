d21f74c6fe7c2895f643a0387ff2a4c24564d671
msedees1807 <marc_edees@hotmail.co.uk>
2017-07-28 11:14:00 +0100

    Update Booking.jsx

--- src/components/pages/Booking.jsx
+++ src/components/pages/Booking.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PriceList from '../../json/prices.json';
+import Locations from '../../json/Locations.json';
 import Paypal from '../functions/Paypal';
 
 export default class Booking extends React.Component {
@@ -7,6 +8,9 @@ export default class Booking extends React.Component {
     constructor(){
         super();
         this.state={
+            filmName: '',
+            date:'',
+            time: '',
             ticketQuantity : 1,
             adultTick : 0,
             kidTick : 0,
@@ -17,17 +21,28 @@ export default class Booking extends React.Component {
             isKidActive: false,
             isSeniorActive: false,
             isTotalActive: false,
-            chosenCinema: '',
+            location: 'Please Select',
             chosenSeats: '',
         };
     }
+    
+    componentWillMount(){
+        this.setState({filmName: localStorage.getItem('filmName')});
+        this.setState({date: localStorage.getItem('date')});
+        this.setState({time: localStorage.getItem('time')});
+        this.setState({location: localStorage.getItem('location')});
+  
+    }
    
+    componentWillUnmount(){
+      
+    }
+    
     increaseTicketQuantity(){
         if (this.state.ticketQuantity > 0 && this.state.ticketQuantity <5){
         this.setState({ticketQuantity : this.state.ticketQuantity+1});
         }
     }
-	
     
     decreaseTicketQuantity(){
         if (this.state.ticketQuantity > 1){
@@ -95,9 +110,9 @@ export default class Booking extends React.Component {
     
     sumTotal(){
         switch (this.state.ticketType){
-            default:       break;
-            case "Adult":  this.sumAdults();break;
-            case "Child":  this.sumKids(); break;
+            case "blank": break;
+            case "Adult": this.sumAdults();break;
+            case "Child": this.sumKids(); break;
             case "Senior": this.sumSeniors(); break;
         }; 
         this.totalPrice();
@@ -108,12 +123,21 @@ export default class Booking extends React.Component {
     }
     
     setChosenCinema(e){
-        this.setState({chosenCinema : e.target.value});
+        this.setState({location : e.target.value});
     }
     
     setChosenSeat(e){
         this.setState({chosenSeats : e.target.value});
     }
+    
+     setChosenDate(e){
+        this.setState({date : e.target.value});
+    }
+    
+    setChosenTime(e){
+        this.setState({time : e.target.value});
+    }
+    
     
     totalPrice(){
         var totalAdultPrice = this.state.adultTick * PriceList.price[0].price;
@@ -124,13 +148,12 @@ export default class Booking extends React.Component {
     }
     
   render() {
-	  
     return (
           <div className = 'bookingSystem'>
             
             <div className = "filmNameDisplay" style = {{backgroundImage : 'url(/img/back.jpg)'}}><br /><br /><br /><br />
                 <h2 className = 'bookingHeading'>Booking</h2>
-                <h3 className = 'bookingHeading2'>Film Name</h3>
+                <h3 className = 'bookingHeading2'>{this.state.filmName}</h3>
             </div>
             
 <div className = 'ticketDetailsSelection'>           
@@ -142,12 +165,46 @@ export default class Booking extends React.Component {
             </td>
             <td>
                 <select className = 'dropdownBooking' onChange = {this.setChosenCinema.bind(this)}>
-                  <option value="Please choose a cinema" selected>Please Select</option>
+                  <option value="Please choose a cinema" selected>{this.state.location}</option>
                   <option value="London">London</option>
                   <option value="Manchester">Manchester</option>
                   <option value="Edinburgh">Edinburgh</option>
                 </select>
             </td>
+            <tr>
+            <td>
+                <label>Day</label>
+            </td>
+            <td>
+                <select className = 'dropdownBooking' onChange = {this.setChosenDate.bind(this)}>
+                <option value="Please choose a cinema" selected>{this.state.date}</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                 <option value="Thursday">Thursday</option>
+                 <option value="Friday">Friday</option>
+                 <option value="Saturday">Saturday</option>
+                 <option value="Sunday">Sunday</option>
+
+                </select>
+            </td>
+            </tr>
+            
+            <tr>
+            <td>
+                <label>Time</label>
+            </td>
+            <td>
+                <select className = 'dropdownBooking' onChange = {this.setChosenTime.bind(this)}>
+                  <option value="Please choose a cinema" selected>{this.state.time}</option>
+                  <option value="12:00pm">12:00pm</option>
+                    <option value="1:00pm">1:00pm</option>
+                    <option value="2:00pm">2:00pm</option>
+                    <option value="3:00pm">3:00pm</option>
+        
+                </select>
+            </td>
+            </tr>
         </div>
     </tr>
     
@@ -194,12 +251,8 @@ export default class Booking extends React.Component {
         </td>
         </div>
     </tr>
-</table>
-
-            
-            
-            
-
+    </table>
+    <br></br>
             
         <div className = "displayTicketSelection">
                   
@@ -207,26 +260,26 @@ export default class Booking extends React.Component {
                 {this.state.isAdultActive ? 
                 <div className = "adultTicks">
                 <p>x {this.state.adultTick} Adult Ticket(s)
-                    <button onClick ={this.clearAdult.bind(this)}>Remove</button>
+                    <button className = "removeButton" onClick ={this.clearAdult.bind(this)}>Remove</button>
                 </p>
                 </div> : ''}
                   
                 {this.state.isKidActive ? 
                 <div className = "kidTicks">  
                 <p>x {this.state.kidTick} Child Ticket(s) 
-                    <button onClick = {this.clearKids.bind(this)}>Remove</button>
+                    <button className = "removeButton" onClick = {this.clearKids.bind(this)}>Remove</button>
                 </p>
                 </div> : ''}
                   
                 {this.state.isSeniorActive ? 
                 <div className = "seniorTicks">
                 <p>x {this.state.seniorTick} Senior Ticket(s) 
-                    <button onClick = {this.clearSeniors.bind(this)}>Remove</button>
+                    <button className = "removeButton" onClick = {this.clearSeniors.bind(this)}>Remove</button>
                 </p>
                 </div> : ''}
             </div>
                   
-                  
+                <br></br>
                 <hr className = 'pageSplit' width = '75%'/>
 
     <table className = 'setOut2'>      
@@ -236,25 +289,23 @@ export default class Booking extends React.Component {
                     <div className = "totalTicks">
                         <h4>You would like {this.state.finalTicketQuantity} ticket(s).</h4>
                         <div className = 'ticketInfo'>
-                        Cinema: {this.state.chosenCinema}<br />
+                        Cinema: {this.state.location}<br />
+                        Time: {this.state.date} at {this.state.time}<br />
                         Seats: {this.state.chosenSeats}<br />
                         Price: £{this.totalPrice()}
                     </div></div> : <h4>Your basket is empty.</h4>}
             </td>
         </tr>
         <tr>
-            <div className = 'reviewAndPayButton'>
+            
+              <div className = 'reviewAndPayButton'>
                     <Paypal price={this.totalPrice()} /> 
                 </div>
         </tr>
     </table>
     </div> 
-            
         
       </div>
-	  
-	  
-	  
     </div>
     );
   }
